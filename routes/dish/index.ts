db13ff6a232829044c35f2abfcb3375051805a6a
jsdevraju <islamrajulearn.dev@gmail.com>
2022-08-17 21:34:14 +0600

    added dish api validation

--- routes/dish/index.ts
+++ routes/dish/index.ts
@@ -1,9 +1,25 @@
 import { Router } from "express";
 import { createDish } from "../../controllers/dish";
 import { adminRole, isAuthenticated } from "../../middleware/auth";
+import { createValidator } from "express-joi-validation";
+import Joi from "joi";
 
 const router = Router();
+const validator = createValidator({});
 
-router.post("/create", isAuthenticated, adminRole("admin"), createDish);
+const dishSchema = Joi.object({
+  name: Joi.string().min(20).max(100).required(),
+  short_description: Joi.string().min(50).max(250).required(),
+  price: Joi.number().required(),
+  image: Joi.string().required(),
+});
+
+router.post(
+  "/create",
+  validator.body(dishSchema),
+  isAuthenticated,
+  adminRole("admin"),
+  createDish
+);
 
 export default router;
