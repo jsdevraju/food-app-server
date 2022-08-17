507318cba0c6706f83f10df28d5bdb52a3bd0613
jsdevraju <islamrajulearn.dev@gmail.com>
2022-08-17 21:36:29 +0600

    added featured api validation

--- routes/featured/index.ts
+++ routes/featured/index.ts
@@ -1,10 +1,25 @@
 import { Router } from "express";
 import { createFeatured, getAllFeatured } from "../../controllers/featured";
 import { adminRole, isAuthenticated } from "../../middleware/auth";
+import { createValidator } from "express-joi-validation";
+import Joi from "joi";
 
 const router = Router();
+const validator = createValidator({});
 
-router.post("/create", isAuthenticated, adminRole("admin"), createFeatured);
+const featuredSchema = Joi.object({
+  name: Joi.string().min(20).max(100).required(),
+  short_description: Joi.string().min(50).max(250).required(),
+  restaurants: Joi.array(),
+});
+
+router.post(
+  "/create",
+  validator.body(featuredSchema),
+  isAuthenticated,
+  adminRole("admin"),
+  createFeatured
+);
 router.get("/get-data", getAllFeatured);
 
 export default router;
